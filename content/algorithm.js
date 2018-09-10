6c39ecf002392484ebc29208d9d19726c7c33b0a
nelsonthegr8 <32414327+nelsonthegr8@users.noreply.github.com>
2018-09-10 01:43:28 -0400

    Javascript
    i messed up i didnt put the ; after the alerts so they were not working sorry for all the commits idk if its bad or not but i remmember you saying somehting about before you commit. but idk anyway i think this will fix it if not ill try in the morning im up too long trying to fix it

--- content/algorithm.js
+++ content/algorithm.js
@@ -1,81 +1,80 @@
-function addition(){
-    var sum = num1+num2;
-    var text = "=";
+function addition() {
     var num1 = Number(prompt("Enter the first number you would like to add."));
+    var sum = num1+num2;
     
     
-    if( isNaN(num1) || num1=="" ){
+    if( isNaN(num1) || num1=== "" ){
 
-        alert("Invalid First number")
+        alert("Invalid First number");
         
     }
 
     var num2 = Number(prompt("Enter the second number you would like to add."));
-     if (isNaN(num2) || num2==""){
-        alert("Invalid Second number")
+     if (isNaN(num2) || num2=== ""){
+        alert("Invalid Second number");
     }
 
-    alert(num1 + "+" +num2+ text+ sum)
+    alert(num1 + "+" +num2+"="+ sum);
 }
 
 function subtraction() {
-    var sum = num1-num2;
-    var text = "=";
-
     var num1 = Number(prompt("Enter the First number you want to Subtract."));
+     var sum = num1-num2;
     
 
-    if( isNaN(num1) || num1=="" ){
+    if( isNaN(num1) || num1=== "" ){
 
-alert("Invalid First number")
+alert("Invalid First number");
             }
 
             var num2 = Number(prompt("Enter the Second number you want to Subtract."));
            
-            if (isNaN(num2) || num2==""){
-            alert("Invalid Second number")
+            if (isNaN(num2) || num2=== ""){
+            alert("Invalid Second number");
                 }
 
-    alert(num1+"-"+num2+text+sum)
+    alert(num1+"-"+num2+"="+sum);
 }
 
 function multiplication(){
-    var sum = num1*num2;
-    var text = "=";
-
-    var num1 = Number(prompt("Enter the First number you want to Multiply."));
     
-    if(  isNaN(num1) || num1=="" ){
+    var num1 = Number(prompt("Enter the First number you want to Multiply."));
+    var sum = num1*num2;
+    
 
-    alert("Invalid First number")
+    
+    
+    if(  isNaN(num1) || num1=== "" ){
+
+    alert("Invalid First number");
         }
     
         var num2 = Number(prompt("Enter the Second number you want to Multiply."));
 
-        if (isNaN(num2) || num2==""){
-    alert("Invalid Second number")
+        if (isNaN(num2) || num2=== ""){
+    alert("Invalid Second number");
 }
 
-    alert(num1+"*"+num2+text+sum)
+    alert(num1+"*"+num2+"="+sum);
 }
 
 function division(){
-    var sum = num1/num2;
-    var text = "=";
+   
 
     var num1 = Number(prompt("Enter the First number you want to Divide."));
+     var sum = num1/num2;
     
 
-    if(  isNaN(num1) || num1=="" ){
+    if(  isNaN(num1) || num1=== "" ){
 
-    alert("Invalid First number")
+    alert("Invalid First number");
     }
 
     var num2 = Number(prompt("Enter the Second number you want to Divide."));
 
-    if (isNaN(num2) || num2==""){
-    alert("Invalid Second number")
+    if (isNaN(num2) || num2=== ""){
+    alert("Invalid Second number");
     }
 
-    alert(num1+"/"+num2+text+sum)
+    alert(num1+"/"+num2+"="+sum);
 }
